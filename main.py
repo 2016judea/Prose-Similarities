edddbdf7d65265043a4c85dbef30cfacdfe68d51
Aidan Jude <aidanjude2016@gmail.com>
2020-03-15 17:12:41 -0500

    additional image

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    graph = build_likeness_graph(os.path.join(os.getcwd(), 'novels', 'classics'), show_graph=True, shortest_path=False)
+    #graph = build_likeness_graph(os.path.join(os.getcwd(), 'novels', 'fitzgerald'), show_graph=True, shortest_path=False)
 
     
     #sentiment_by_parts(os.path.join(os.getcwd(), 'novels', 'ThisSideOfParadise.txt'), scale=4, jump=2)
@@ -19,12 +19,13 @@ def main():
     
     
     novels_to_compare = [
-        os.path.join(os.getcwd(), 'novels', 'ThisSideOfParadise.txt'),
-        os.path.join(os.getcwd(), 'novels', 'TheBeautifulAndDamned.txt'),
-        os.path.join(os.getcwd(), 'novels', 'TheGreatGatsby.txt'),
-        os.path.join(os.getcwd(), 'novels', 'TenderIsTheNight.txt')
+        os.path.join(os.getcwd(), 'novels', 'fitzgerald', 'ThisSideOfParadise.txt'),
+        os.path.join(os.getcwd(), 'novels', 'fitzgerald', 'TheBeautifulAndDamned.txt'),
+        os.path.join(os.getcwd(), 'novels', 'fitzgerald', 'TheGreatGatsby.txt'),
+        os.path.join(os.getcwd(), 'novels', 'fitzgerald', 'TenderIsTheNight.txt')
     ]
-    #sentiment_multiple_novels(novels_to_compare, 6)
+
+    sentiment_multiple_novels(novels_to_compare, 6)
 
 if __name__ == "__main__":
     main()
